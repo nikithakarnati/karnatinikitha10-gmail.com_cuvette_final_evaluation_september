a1f6bf11adaca524284a851bf009f114341f4344
nikithakarnati <160767116+nikithakarnati@users.noreply.github.com>
2024-10-02 22:59:23 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import './styles/index.css';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/repo-name"> {/* Replace "repo-name" with your actual repository name */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
